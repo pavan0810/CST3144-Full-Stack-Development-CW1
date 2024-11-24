dfda9b669fc0eb80ceea677f6b9f9f6a167048a2
pavan0810 <PH526@live.mdx.ac.uk>
2024-11-25 00:29:54 +0400

    adding connection uri for mongodb

--- express app/app.js
+++ express app/app.js
@@ -1,6 +1,23 @@
 var http = require('http');
 var express = require('express');
 var cors = require('cors');
+var propertiesReader = require('properties-reader');
+var path = require('path')
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+var propertiesPath = path.resolve(__dirname, "conf/db.properties")
+var properties = propertiesReader(propertiesPath);
+
+var dbPrefix = properties.get("db.prefix");
+var dbUsername = encodeURIComponent(properties.get("db.user"));
+var dbPwd = encodeURIComponent(properties.get("db.pwd"));
+var dbName = properties.get("db.dbName");
+var dbUrl = properties.get("db.dbUrl");
+var dbParams = properties.get("db.params");
+const uri = dbPrefix + dbUsername + ":" + dbPwd + dbUrl + dbParams;
+const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+var db = client.db(dbName);
+
 var cart = []
 var courses = [
     {
